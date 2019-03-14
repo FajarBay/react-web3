30231d377162322686626704d0d7854211a39fff
FajarBay <fajarbay.github.com>
2019-03-14 08:05:39 +0700

    Updates

---
+++ precache-manifest.f2208d10b00db2e2f39cbec37be60544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "31d5bc0d8e99739a6488",
+    "url": "/react-web3/static/js/runtime~main.a9207d26.js"
+  },
+  {
+    "revision": "512222036169658a4c25",
+    "url": "/react-web3/static/js/main.982fde1a.chunk.js"
+  },
+  {
+    "revision": "5711b84e88abdf95a81e",
+    "url": "/react-web3/static/js/2.f4c73c90.chunk.js"
+  },
+  {
+    "revision": "512222036169658a4c25",
+    "url": "/react-web3/static/css/main.e331993c.chunk.css"
+  },
+  {
+    "revision": "b89ac6569b5fea2fccd45cf0d5061dec",
+    "url": "/react-web3/index.html"
+  }
+];
